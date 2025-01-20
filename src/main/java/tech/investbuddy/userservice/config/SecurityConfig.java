84e61d2f36348a695e220127b841cf93dc5da623
ABAKHAR ABDESSAMAD <153305975+ABAKHAR721@users.noreply.github.com>
2025-01-20 02:31:32 +0100

    Update SecurityConfig.java

--- src/main/java/tech/investbuddy/userservice/config/SecurityConfig.java
+++ src/main/java/tech/investbuddy/userservice/config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package tech.investbuddy.userservice.config;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable() // Disable CSRF for simplicity
-            .authorizeRequests()
-            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow preflight requests
-            .anyRequest().authenticated(); // Secure other endpoints
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for REST APIs
+                .cors(Customizer.withDefaults()) // Enable default CORS configuration
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Public access for OPTIONS requests
+                        .anyRequest().authenticated() // All other requests require authentication
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())) // Configure OAuth2 Resource Server
+                .build();
     }
 }
