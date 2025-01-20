4fd37a941567fe547d12752951688cbfee94c7f3
ABAKHAR ABDESSAMAD <153305975+ABAKHAR721@users.noreply.github.com>
2025-01-20 02:13:08 +0100

    Update WebConfig.java

--- src/main/java/tech/investbuddy/userservice/config/WebConfig.java
+++ src/main/java/tech/investbuddy/userservice/config/WebConfig.java
@@ -1,19 +1,15 @@
-package tech.investbuddy.apigateway.config;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                // Allow requests from Angular frontend running on localhost:4200
-                registry.addMapping("/").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
-            }
-        };
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:4200") // Allow requests from Angular app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (e.g., cookies)
     }
 }
